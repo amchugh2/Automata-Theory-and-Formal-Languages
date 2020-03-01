e9584812741bf66471efc534cfe6792532835b98
Abigail McHugh <amchugh2@binghamton.edu>
2020-03-01 15:21:49 -0500

    could not find or load main class error

--- mchugh_p1/mchugh_p1.java
+++ mchugh_p1/mchugh_p1.java
@@ -13,142 +13,144 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-class FA {
-	// Create complete list for FA
-	public LinkedList<LinkedList<String>> FA = new LinkedList<LinkedList<String>>();
+public class mchugh_p1{
 
-	// Map of valid valid_transitions
-	public TreeMap<String, ArrayList<String>> valid_transitions = new TreeMap<String, ArrayList<String>>();
-	public String start;
+	class FA {
+		// Create complete list for FA
+		public LinkedList<LinkedList<String>> FA = new LinkedList<LinkedList<String>>();
 
-	// List of Accept states
-	public ArrayList<Integer> accept_state = new ArrayList<Integer>();
+		// Map of valid valid_transitions
+		public TreeMap<String, ArrayList<String>> valid_transitions = new TreeMap<String, ArrayList<String>>();
+		public String start;
 
-	// Array of potential accept states - 1000 valid_transitions
-	public boolean[] accept_states = new boolean[1000];
+		// List of Accept states
+		public ArrayList<Integer> accept_state = new ArrayList<Integer>();
 
-	// Treesets to hold accepted_strings and rejected_strings strings
-	public TreeSet<String> accepted_strings = new TreeSet<String>();
-	public TreeSet<String> rejected_strings = new TreeSet<String>();
+		// Array of potential accept states - 1000 valid_transitions
+		public boolean[] accept_states = new boolean[1000];
 
-	// Bool - default mode = accept
-	public boolean accept = true;
+		// Treesets to hold accepted_strings and rejected_strings strings
+		public TreeSet<String> accepted_strings = new TreeSet<String>();
+		public TreeSet<String> rejected_strings = new TreeSet<String>();
 
-	// NFA generator
-	public void NFA_generator() {
-		// While FA is not empty
-		while(FA.size() != 0) {
-			// Get input state num
-			String input_state = FA.get(0).get(1);
+		// Bool - default mode = accept
+		public boolean accept = true;
 
-			if(input_state.isEmpty() == true) { // INPUT STATE IS EMPTY
-				// get first state 
-				String first_state = FA.get(0).get(0);
-				//System.out.println(first_state);
-				int int_NS = Integer.parseInt(first_state);
+		// NFA generator
+		public void NFA_generator() {
+			// While FA is not empty
+			while(FA.size() != 0) {
+				// Get input state num
+				String input_state = FA.get(0).get(1);
 
-				if(accept_states[int_NS] == false) {
-					rejected_strings.add(first_state);
+				if(input_state.isEmpty() == true) { // INPUT STATE IS EMPTY
+					// get first state
+					String first_state = FA.get(0).get(0);
+					//System.out.println(first_state);
+					int int_NS = Integer.parseInt(first_state);
+
+					if(accept_states[int_NS] == false) {
+						rejected_strings.add(first_state);
+					}
+					else {
+						accepted_strings.add(first_state);
+					}
 				}
+				// If input state is not empty
 				else {
-					accepted_strings.add(first_state);
-				}
-			}
-			// If input state is not empty
-			else {
-				// Create key
-				// Get next transition
-				String next = FA.get(0).get(0) + FA.get(0).get(1).charAt(0);
-				String status = input_state.substring(1);
-				//System.out.println(e);
+					// Create key
+					// Get next transition
+					String next = FA.get(0).get(0) + FA.get(0).get(1).charAt(0);
+					String status = input_state.substring(1);
+					//System.out.println(e);
 
-				if(!valid_transitions.containsKey(next)) accept = false;
+					if(!valid_transitions.containsKey(next)) accept = false;
 
-				else {
-					//If valid transition
-					// Get list of states beginning with next
-					LinkedList<String> states = new LinkedList<>(valid_transitions.get(next));
+					else {
+						//If valid transition
+						// Get list of states beginning with next
+						LinkedList<String> states = new LinkedList<>(valid_transitions.get(next));
 
-					// Iterate thru list of new states
-					for(int i = 0; i < states.size(); i++) {
-						// New state
-						String new_state = states.get(i);
-						// Integer to check for pool
-						int int_NS = Integer.parseInt(new_state);                        
-						// if status is empty
-						if(status.isEmpty() == true) {
-							// if in accept state
-							if(accept_states[int_NS]) {
-								// if is not already included in accepted strings
-								if(accepted_strings.contains(new_state) == false) {
-									// Add
-									accepted_strings.add(new_state);
+						// Iterate thru list of new states
+						for(int i = 0; i < states.size(); i++) {
+							// New state
+							String new_state = states.get(i);
+							// Integer to check for pool
+							int int_NS = Integer.parseInt(new_state);
+							// if status is empty
+							if(status.isEmpty() == true) {
+								// if in accept state
+								if(accept_states[int_NS]) {
+									// if is not already included in accepted strings
+									if(accepted_strings.contains(new_state) == false) {
+										// Add
+										accepted_strings.add(new_state);
+									}
 								}
-							}
-							// if NOT in accept states and empty
-							if(accept_states[int_NS] == false && accepted_strings.size() == 0) {
-								// if NOT in rejected strings already, add it
-								if(rejected_strings.contains(new_state) == false) {
-									rejected_strings.add(new_state);
+								// if NOT in accept states and empty
+								if(accept_states[int_NS] == false && accepted_strings.size() == 0) {
+									// if NOT in rejected strings already, add it
+									if(rejected_strings.contains(new_state) == false) {
+										rejected_strings.add(new_state);
+									}
 								}
+							} else {
+								// update and add to FA
+								LinkedList<String> updated = new LinkedList<String>(Arrays.asList(states.get(i), status));
+								FA.add(updated);
 							}
-						} else {
-							// update and add to FA
-							LinkedList<String> updated = new LinkedList<String>(Arrays.asList(states.get(i), status));
-							FA.add(updated);
 						}
 					}
-				} 
+				}
+				// REMOVE
+				FA.remove(0);
 			}
-			// REMOVE
-			FA.remove(0);
+		}
+
+		// set accept state
+		public void set_accept_state(String state) {
+			// Change to int
+			int int_state = Integer.parseInt(state);
+			// Set bool list
+			accept_states[int_state] = true;
+			// Add to list of accept states
+			accept_state.add(int_state);
+		}
+
+		// set start state
+		public void set_start_state(String input, String n) {
+			start = input;
+			FA.add(new LinkedList<String>(Arrays.asList(start,n)));
+		}
+
+		// set transitions
+		public void set_valid_transition(String input, String end) {
+			if(valid_transitions.containsKey(input) == false) {
+				valid_transitions.put(input, new ArrayList<String>());
+			}
+			valid_transitions.get(input).add(end);
+		}
+
+		// Formatting
+		public void pretty_print() {
+			if(accepted_strings.size() > 0) {
+				System.out.print("accept ");
+				for(String s : accepted_strings) {
+					System.out.print(s + " ");
+				}
+			} else {
+				System.out.print("reject ");
+				for(String s : rejected_strings) {
+					System.out.print(s + " ");
+				}
+			}
 		}
 	}
 
-	// set accept state
-	public void set_accept_state(String state) {
-		// Change to int
-		int int_state = Integer.parseInt(state);
-		// Set bool list
-		accept_states[int_state] = true;
-		// Add to list of accept states
-		accept_state.add(int_state);
-	}
-
-	// set start state
-	public void set_start_state(String input, String n) {
-		start = input;
-		FA.add(new LinkedList<String>(Arrays.asList(start,n)));
-	}
-
-	// set transitions
-	public void set_valid_transition(String input, String end) {
-		if(valid_transitions.containsKey(input) == false) {
-			valid_transitions.put(input, new ArrayList<String>());
-		}
-		valid_transitions.get(input).add(end);
-	}
-
-	// Formatting
-	public void pretty_print() {
-		if(accepted_strings.size() > 0) {
-			System.out.print("accept ");
-			for(String s : accepted_strings) {
-				System.out.print(s + " ");
-			}
-		} else {
-			System.out.print("reject ");
-			for(String s : rejected_strings) {
-				System.out.print(s + " ");
-			}
-		}
-	}
-}
-
-public class mchugh_p1 {
 	public static void main(String[] args) {
 		// Create NFA
-		FA nfa = new FA();
+		mchugh_p1 p1 = new mchugh_p1();
+		FA nfa = p1.new FA();
 		// Initialize file and input
 		//File file;
 		String input = args[1];
@@ -218,8 +220,8 @@ public class mchugh_p1 {
 						else if(next_type.equalsIgnoreCase("accept")) {
 							nfa.set_accept_state(state);
 						}
-					} 
-				} 
+					}
+				}
 			}
 
 			//Create NFA & Format Print
